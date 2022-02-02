4cede18ac2551ae2fd8296f5531bf27e3a63905d
cjwe <christajwest@gmail.com>
2022-02-02 14:06:33 +0100

    Event details tests passed

--- src/Event.js
+++ src/Event.js
@@ -1,12 +1,48 @@
 import React, { Component } from 'react';
 
 class Event extends Component {
+  state = {
+    collapsed: true,
+  };
+
+  handleClick = () => {
+    this.setState({
+      collapsed: !this.state.collapsed,
+    });
+  };
+
   render() {
     const { event } = this.props;
+    const { collapsed } = this.state;
     return (
       <div className="event">
         <h2 className="summary">{event.summary}</h2>
-        <p className="startTime"> {event.dateTime}</p>
+        <p className="startTime">
+          {' '}
+          {event.dateTime} <span className="timeZone"> ({event.timeZone})</span>
+        </p>
+        <p className="location">
+          @{event.summary} | {event.location}
+        </p>
+        if(collapsed === true)
+        {
+          <button className="show-details" onClick={this.handleClick}>
+            Show details
+          </button>
+        }
+        else
+        {
+          <div className="details-view">
+            <h4 className="details-header"> About event:</h4>
+            <a className="htmlLink" href={event.htmlLink}>
+              See details on Google Calendar
+            </a>
+            <p className="description">{event.description}</p>
+            <button className="hide-details" onClick={this.handleClick}>
+              Hide details
+            </button>
+          </div>
+        }
       </div>
     );
   }
--- src/__tests__/Event.test.js
+++ src/__tests__/Event.test.js
@@ -11,6 +11,7 @@ describe('<Event /> component', () => {
     EventWrapper = shallow(<Event event={mockData} />);
   });
 
+  // Event Details Tests
   test('render event', () => {
     expect(EventWrapper.find('.event')).toHaveLength(1);
   });
@@ -21,5 +22,46 @@ describe('<Event /> component', () => {
 
   test('render event start time', () => {
     expect(EventWrapper.find('.startTime')).toHaveLength(1);
+  });
+
+  test('render event time zone', () => {
+    expect(EventWrapper.find('.timeZone')).toHaveLength(1);
+  });
+
+  test('render event location', () => {
+    expect(EventWrapper.find('.location')).toHaveLength(1);
+  });
+
+  test('render event link', () => {
+    expect(EventWrapper.find('.htmlLink')).toHaveLength(1);
+  });
+
+  test('render event description', () => {
+    expect(EventWrapper.find('.description')).toHaveLength(1);
+  });
+
+  //Collapse & Button tests
+  test('render show details button', () => {
+    expect(EventWrapper.find('.show-details')).toHaveLength(1);
+  });
+
+  test('clicking show-details button shows event details', () => {
+    EventWrapper.setState({
+      collapsed: true,
+    });
+    EventWrapper.find('.show-details').simulate('click');
+    expect(EventWrapper.state('collapsed')).toBe(false);
+  });
+
+  test('render hide details button', () => {
+    expect(EventWrapper.find('.hide-details')).toHaveLength(1);
+  });
+
+  test('clicking hide-details button hides event details', () => {
+    EventWrapper.setState({
+      collapsed: false,
+    });
+    EventWrapper.find('.hide-details').simulate('click');
+    expect(EventWrapper.state('collapsed')).toBe(true);
   });
 });
