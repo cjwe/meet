9e4629cf7861ecd2ef48254144adede328a43078
cjwe <christajwest@gmail.com>
2022-02-18 12:51:45 +0100

    Added welcome screen and privacy styling

--- src/WelcomeScreen.jsx
+++ src/WelcomeScreen.jsx
@@ -1,48 +1,50 @@
 import React from 'react';
 import './WelcomeScreen.css';
-import { Container, Row, Col } from 'react-bootstrap';
 
 function WelcomeScreen(props) {
   return props.showWelcomeScreen ? (
-    <Container>
-      <Row>
-        <Col>
-          <div className="WelcomeScreen">
-            <h1 className="header">Welcome to the Meet app</h1>
-            <h4>
-              Log in to see upcoming events around the world for full-stack
-              developers.
-            </h4>
-            <div className="button_cont" align="center">
-              <div className="google-btn">
-                <div className="google-icon-wrapper">
-                  <img
-                    className="google-icon"
-                    src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
-                    alt="Google sign-in"
-                  />
-                </div>
-                <button
-                  onClick={() => {
-                    props.getAccessToken();
-                  }}
-                  rel="nofollow noopener"
-                  class="btn-text"
-                >
-                  <b>Sign in with google</b>
-                </button>
-              </div>
+    <div className="WelcomeScreen">
+      <video autoPlay loop muted id="video">
+        <source
+          src="https://assets.mixkit.co/videos/preview/mixkit-man-and-woman-commenting-on-an-image-on-a-laptop-4904-large.mp4"
+          type="video/mp4"
+        />
+      </video>
+      <div class="overlay"></div>
+      <div className="container">
+        <h1 className="header">
+          Welcome to <br /> <span className="title-text">Meet App</span>
+        </h1>
+        <h4>Log in to see upcoming, world-wide events for developers.</h4>
+        <div className="button_cont" align="center">
+          <div className="google-btn">
+            <div className="google-icon-wrapper">
+              <img
+                className="google-icon"
+                src="https://upload.wikimedia.org/wikipedia/commons/5/53/Google_%22G%22_Logo.svg"
+                alt="Google sign-in"
+              />
             </div>
-            <a
-              href="https://cjwe.github.io/meet/privacy.html"
+            <button
+              onClick={() => {
+                props.getAccessToken();
+              }}
               rel="nofollow noopener"
+              class="btn-text"
             >
-              Privacy policy
-            </a>
+              <b>Sign in with google</b>
+            </button>
           </div>
-        </Col>
-      </Row>
-    </Container>
+        </div>
+        <a
+          className="htmlLink"
+          href="https://cjwe.github.io/meet/privacy.html"
+          rel="nofollow noopener"
+        >
+          Privacy policy
+        </a>
+      </div>
+    </div>
   ) : null;
 }
 
